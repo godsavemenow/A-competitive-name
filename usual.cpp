c1febdc76aee3f1d846a08e2886ea15b59d6bd83
godsavemenow <lvjs@cin.ufpe.br>
2019-12-23 02:19:33 -0300

    usual file

---
+++ usual.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main() {  
+cin.tie(0);
+ios::sync_with_stdio(0);
+
+return(0);
+}
+//g++ usual.cpp -std=c++11 -Wfatal-errors -o test
+//test / test >> output.txt
