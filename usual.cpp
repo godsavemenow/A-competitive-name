41e98887f5299513c1d6c11bc1a03e6ea313439a
Vitor Sousa <vss2@cin.ufpe.br>
2020-10-31 17:20:42 -0300

    Fix de bugs no bubble.cpp, e identacao e includes

--- usual.cpp
+++ usual.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <bits/stdc++.h>
-using namespace std;
-int main() {  
-cin.tie(0);
-ios::sync_with_stdio(0);
 
-return(0);
+using namespace std;
+
+int main() {  
+        cin.tie(0);
+        ios::sync_with_stdio(0);
+
+        return(0);
 }
+
 //g++ usual.cpp -std=c++11 -Wfatal-errors -o test
 //test / test >> output.txt
 // :)
